063409113dc4cd890e9c8c8051b2d72f06f767dd
ggujangi <asmwj3@naver.com>
2025-04-03 18:04:12 +0900

    #1 서브 화면 생성

---
+++ app/src/main/java/com/ggu/media/ui/media/MediaScreen.kt
@@ -0,0 +1,11 @@
+package com.ggu.media.ui.media
+
+import androidx.compose.runtime.Composable
+import com.ggu.media.Greeting
+
+@Composable
+fun MediaScreen() {
+    Greeting(
+        name = "Media",
+    )
+}
---
+++ app/src/main/java/com/ggu/media/ui/story/StoryScreen.kt
@@ -0,0 +1,11 @@
+package com.ggu.media.ui.story
+
+import androidx.compose.runtime.Composable
+import com.ggu.media.Greeting
+
+@Composable
+fun StoryScreen() {
+    Greeting(
+        name = "Story",
+    )
+}
